b62c99abc188f6789c89bb0b688d3fd4f8fffc16
Analia Mok <axm2580@g.rit.edu>
2018-04-28 09:52:48 -0400

    Removed default demo theme

--- storage/cms/cache/39/1a/features.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5ae47a6e99d6c323226707_980f258f57aff757d49638e074e4bb38Class extends \Cms\Classes\PageCode
-{
-
-}
--- storage/cms/cache/5c/8f/navbar.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5ae47a6ea9980767222359_248361397b6fb6f1f075e8e1a12910beClass extends Cms\Classes\PartialCode
-{
-
-}
